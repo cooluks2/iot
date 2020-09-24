49eef764abc40f906b44ad65014e196c41c40f68
cooluks2 <cooluks2@gmail.com>
2020-09-24 17:53:08 +0900

    폴더 정리

--- 02.device/c++/chapter7/ex05_smart_pointer.cpp
+++ 02.device/c++/chapter7/ex05_smart_pointer.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main()
 {
     unique_ptr<int[]> buf(new int[10]);
+    // unique_ptr<int[]> buf = new int[10]; // error
     for (int i = 0; i < 10; i++)
     {
-        buf[i] = i;
+        buf[i] = i;  // *(buf + i) = i; 와 동일
     }
-    
+
     for (int i = 0; i < 10; i++)
     {
         cout << buf[i] << " ";
